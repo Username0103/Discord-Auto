b6eb53801efdd27207465af9d9b47679d03563e9
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-05-26 14:46:02 -0300

    .

---
+++ main.py
@@ -0,0 +1,5 @@
+
+# i probably want to have some kind of on event call when a message gets sent into the server
+# i probably want to be able to hook this up to my browser
+# i probably want to have a look in the HTML source of discord to see how to identify a message and its content
+
